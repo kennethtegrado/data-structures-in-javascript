8b687c75d6d482f925a8828ff1632b7972f297b7
Kenneth Tegrado <tegradok@gmail.com>
2022-03-30 20:15:36 +0800

    Finished implementation of the inorder dfs

--- data-structures/trees/traversal/dfs.js
+++ data-structures/trees/traversal/dfs.js
@@ -1,27 +1,63 @@
 function preOrder(tree) {
     const visited = [];
-    const node = tree.root;
 
+    // Helper recursive function for traversing our tree
     const traverse = function (node) {
+        // Push our node to the visited nodes
         visited.push(node.value);
+
+        // Recursive call
         if (node.left) traverse(node.left);
         if (node.right) traverse(node.right);
     };
 
-    traverse(node);
+    // Call traverse
+    traverse(tree.root);
     return visited;
 }
 
 function postOrder(tree) {
     const visited = [];
-    const node = tree.root;
 
     const traverse = function (node) {
+        // Recursive Call
         if (node.left) traverse(node.left);
         if (node.right) traverse(node.right);
+
+        // Push our node to the visited array
         visited.push(node.value);
     };
 
-    traverse(node);
+    // Function call
+    traverse(tree.root);
     return visited;
 }
+
+function inOrder(tree) {
+    const visited = [];
+
+    const traverse = function (node) {
+        // Recursive Call
+        if (node.left) traverse(node.left);
+
+        // Push our node to the visited array
+        visited.push(node.value);
+
+        // Recursive call
+        if (node.right) traverse(node.right);
+    };
+
+    // Function call
+    traverse(tree.root);
+    return visited;
+}
+
+// function stackPreOrder(tree) {
+//     const stack = [],
+//         visited = [];
+//     let node = tree.root;
+//     stack.push(node);
+//     while (stack.length) {
+//         node = tree
+//     }
+// }
