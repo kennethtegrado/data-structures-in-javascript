f0522dece7b48f5389c075499c885d8842a84b9a
Kenneth Tegrado <tegradok@gmail.com>
2022-04-01 22:21:15 +0800

    added implementation of remove edge

--- data-structures/heaps/graph/Graph.js
+++ data-structures/heaps/graph/Graph.js
@@ -20,4 +20,20 @@ class Graph {
 
         this.adjacencyList[vertex2].push(vertex1);
     }
+
+    // REMOVE AN EDGE
+    removeEdge(v1, v2) {
+        // Check if both of the vertices exist
+        if (this.adjacencyList[v1] && this.adjacencyList[v2]) {
+            // Remove the connection in vertice 1 and vertice 2
+            this.adjacencyList[v1] = this.adjacencyList[v1].filter(
+                (item) => item !== v2
+            );
+
+            // Remove the connection in vertice 2 and vertice 1
+            this.adjacencyList[v2] = this.adjacencyList[v2].filter(
+                (item) => item !== v1
+            );
+        }
+    }
 }
