dfbbc7587c40fcfd0441f125676e299bd976859e
Kenneth Tegrado <tegradok@gmail.com>
2022-04-02 14:51:09 +0800

    initialized weighted undirectional graph

--- data-structures/graph/undirected/WeightedGraph.js
+++ data-structures/graph/undirected/WeightedGraph.js
@@ -2,4 +2,24 @@ class WeightedGraph {
     constructor() {
         this.adjacencyList = {};
     }
+
+    // ADD VERTEX IMPLEMENTATION
+    addVertex(vertex) {
+        // Check if the vertex does not exist in our adjacency list
+        if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+    }
+
+    // ADD EDGE IMPLEMENTATION
+    addEdge(vertex1, vertex2, weight) {
+        // Check if any of our vertex exists
+        if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
+            // Add an edge to our vertex 1
+            this.adjacencyList[vertex1].push({ node: vertex2, weight });
+
+            // Add an edge to our vertex 2
+            this.adjacencyList[vertex2].push({ node: vertex1, weight });
+        }
+    }
 }
+
+// SAMPLE IMPLEMENTATION
