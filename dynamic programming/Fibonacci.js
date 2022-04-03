a3c68006946abcc9f173b166cb4048ec2710f4ab
Kenneth Tegrado <tegradok@gmail.com>
2022-04-03 19:11:38 +0800

    added implementation of bottom up fibonacci sequence approach

--- dynamic programming/Fibonacci.js
+++ dynamic programming/Fibonacci.js
@@ -14,3 +14,27 @@ console.log(fibo(10));
 console.log(fibo(15));
 console.log(fibo(25));
 console.log(fibo(45));
+
+// BETTER PERFORMANCE THAN TOP UP
+// BOTTOM UP APPROACH
+const bottomFibonacci = (n) => {
+    // GUARD CLAUSE
+    // Check if n is less than or equal to 2
+    if (n <= 2) return 1;
+
+    // Array for fast random access
+    const fiboNacciTable = [undefined, 1, 1];
+
+    // Iterate every sequence
+    for (let i = 3; i <= n; i++)
+        fiboNacciTable[i] = fiboNacciTable[i - 1] + fiboNacciTable[i - 2];
+
+    // Return the fibonacci value
+    return fiboNacciTable[n];
+};
+
+console.log(bottomFibonacci(5));
+console.log(bottomFibonacci(10));
+console.log(bottomFibonacci(15));
+console.log(bottomFibonacci(25));
+console.log(bottomFibonacci(45));
